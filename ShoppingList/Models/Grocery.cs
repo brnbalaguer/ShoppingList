b56ae90410e6c0a064e515f675bd5248b655c864
Bruno <Bruno@PC-SALA>
2024-02-03 12:55:45 -0300

    Adicionar arquivos de projeto.

---
+++ ShoppingList/Models/Grocery.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoppingList.Models
+{
+    public class Grocery
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool Purchased { get; set; }
+
+
+    }
+}
---
+++ ShoppingList/ShoppingListContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingList.Models;
+
+namespace ShoppingList
+{
+    public class ShoppingListContext : DbContext
+    {
+        public ShoppingListContext(DbContextOptions<ShoppingListContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Grocery> Grocery { get; set; }
+    }
+}
